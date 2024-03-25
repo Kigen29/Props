8b6e0b592cae99ee55f2ce25730898f33467be3a
Kigen29 <emmanuel.kemboi@strathmore.edu>
2024-03-25 16:02:45 +0200

    fix: fixed question 1

--- reactapp/src/App.tsx
+++ reactapp/src/App.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
-import './App.css';
+import "./App.css";
 import Counter from './component/Counter';
+import PropDrilling from './component/PropDrilling';
+import Context from './component/Context';
+import Form from './component/Form';
+import ShoppingApp from './component/ShoppingApp';
+
 
 function App() {
   return (
-    <div className="App">
-      < Counter initialCount={5} />                                 
+    <div>
+      
+      
+      <Context />
+      <Form />
+      <ShoppingApp />
     </div>
   );
 }
 
+//Add props to Counter
+//initialCount via TypeScript
 
 
-export default App;
+export default App;
